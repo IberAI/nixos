648f3bb31776e488743303088565cd2db3f65900
IberAI <ilteber.dover@gmail.com>
2026-01-08 00:40:54 -0500

    Updated flake

--- home/dev/android.nix
+++ home/dev/android.nix
@@ -1,21 +1,25 @@
 { config, pkgs, lib, ... }:
 
 let
-  androidSdk = pkgs.androidenv.androidPkgs.androidsdk;
+  androidComposition = pkgs.androidenv.composeAndroidPackages {
+    includeEmulator = false;
+    includeSystemImages = false;
+
+    # Keep it minimal: pick ONE platform + ONE build-tools version
+    platformVersions = [ "34" ];
+    buildToolsVersions = [ "34.0.0" ];
+
+    # Optional (leave out unless you hit missing-cmake errors)
+    # cmakeVersions = [ "3.22.1" ];
+  };
+
+  androidSdk = androidComposition.androidsdk;
   sdkRoot = "${androidSdk}/libexec/android-sdk";
 in
 {
   home.packages = with pkgs; [
-    # SDK via Nix (includes platform-tools/adb)
     androidSdk
-
-    # Usual deps for RN/Expo + Gradle
-    jdk17
-    nodejs_20
     watchman
-    git
-    unzip
-    zip
     cmake
     gnumake
     ninja
@@ -29,12 +33,8 @@ in
 
   home.sessionPath = [
     "${sdkRoot}/platform-tools"
-    "${sdkRoot}/emulator"
     "${sdkRoot}/cmdline-tools/latest/bin"
+    # remove emulator path since we’re not installing it:
+    # "${sdkRoot}/emulator"
   ];
-
-  # Optional: helps some Gradle/AGP builds on NixOS (aapt2 override)
-  # If you run into aapt2 errors, enable this.
-  # home.sessionVariables.GRADLE_OPTS =
-  #   "-Dorg.gradle.project.android.aapt2FromMavenOverride=${sdkRoot}/build-tools/28.0.3/aapt2";
 }
