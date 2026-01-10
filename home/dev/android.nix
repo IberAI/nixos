70950223dd56838931e0fcc9a30c1640b976fff5
IberAI <ilteber.dover@gmail.com>
2026-01-10 14:59:29 -0500

    Updated android stuff

--- home/dev/android.nix
+++ home/dev/android.nix
@@ -4,17 +4,14 @@ let
   jdk = pkgs.jdk17;
 
   androidComposition = pkgs.androidenv.composeAndroidPackages {
-    # Match what Expo/Gradle is asking for in your log
     platformVersions   = [ "36" ];
     buildToolsVersions = [ "36.0.0" ];
 
-    # Preinstall NDK so Gradle doesn't try to download it into /nix/store
     includeNDK = true;
     ndkVersions = [ "27.1.12297006" ];
 
-    # optional, but commonly helpful
-    # includeCmake = true;
-    # cmakeVersions = [ "3.22.1" ];
+    includeCmake = true;
+    cmakeVersions = [ "3.22.1" ];
 
     toolsVersion = null;
   };
@@ -25,7 +22,7 @@ in
   home.packages = with pkgs; [
     jdk
     androidComposition.androidsdk
-    android-tools   # adb
+    android-tools # adb
     watchman
     pkg-config
     gnumake
@@ -34,10 +31,9 @@ in
     rsync
   ];
 
-  # Make sure tooling finds the Nix SDK without guessing
   home.sessionVariables = {
     ANDROID_HOME = storeSdkRoot;
     ANDROID_SDK_ROOT = storeSdkRoot;
-    JAVA_HOME = jdk;
+    JAVA_HOME = "${jdk.home}";
   };
 }
