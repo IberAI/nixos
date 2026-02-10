f18c9fa4b8921b2bdab3977b977dbd5a26f037d3
IberAI <ilteber.dover@gmail.com>
2026-02-10 16:28:36 -0500

    Emacs c indentation rules config got added

--- home/dev/emacs.nix
+++ home/dev/emacs.nix
@@ -1,17 +1,26 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
+
 {
   programs.emacs = {
     enable = true;
     package = pkgs.emacs-gtk;
   };
 
+  # Main config in XDG location
   xdg.configFile."emacs/init.el".text = ''
-    (setq-default indent-tabs-mode nil)
-    (setq-default tab-width 4)
+    (setq-default indent-tabs-mode nil
+                  tab-width 4)
 
     (add-hook 'c-mode-hook
               (lambda ()
-                (setq c-basic-offset 4)
-                (setq indent-tabs-mode nil)))
+                (setq c-basic-offset 4
+                      indent-tabs-mode nil)))
   '';
+
+  # Ensure Emacs loads it (Emacs always reads ~/.emacs.d/init.el by default)
+  home.file.".emacs.d/init.el".source =
+    config.xdg.configFile."emacs/init.el".source;
+
+  # (Optional but recommended) create the directory explicitly
+  home.file.".emacs.d/.keep".text = "";
 }
