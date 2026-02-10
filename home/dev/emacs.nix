605122b41b47e8b243a389d1827a9eb492824177
IberAI <ilteber.dover@gmail.com>
2026-02-10 16:04:26 -0500

    Emacs c indentation rules config got added

--- home/dev/emacs.nix
+++ home/dev/emacs.nix
@@ -4,4 +4,14 @@
     enable = true;
     package = pkgs.emacs-gtk;
   };
+
+  xdg.configFile."emacs/init.el".text = ''
+    (setq-default indent-tabs-mode nil)
+    (setq-default tab-width 4)
+
+    (add-hook 'c-mode-hook
+              (lambda ()
+                (setq c-basic-offset 4)
+                (setq indent-tabs-mode nil)))
+  '';
 }
