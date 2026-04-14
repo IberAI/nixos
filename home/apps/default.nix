11995282b7a16c8b2abb99b1e513b4a6567e483b
IberAI <ilteber.dover@gmail.com>
2026-04-14 13:54:52 -0400

    Added small emacs config

--- home/apps/default.nix
+++ home/apps/default.nix
@@ -6,6 +6,7 @@
   ];
 
   home.packages = with pkgs; [
+    brave
     wireshark
     kicad
     mpv
--- home/dev/emacs.nix
+++ home/dev/emacs.nix
@@ -10,46 +10,48 @@
 
   # Main config in XDG location
   xdg.configFile."emacs/init.el".text = ''
-    ;; Basic editing defaults
-    (setq-default indent-tabs-mode nil
-                  tab-width 4)
+      ;; Basic editing defaults
+      (setq-default indent-tabs-mode nil
+                    tab-width 4)
 
-    ;; Start Emacs in dired at ~/Development
-    (setq initial-buffer-choice
-          (lambda () (dired-noselect "~/Development")))
+      ;; Start Emacs in dired at ~/Development
+      (setq initial-buffer-choice
+            (lambda () (dired-noselect "~/Development")))
 
-    ;; Theme
-    (load-theme 'modus-vivendi t)
-
-    ;; Small UI cleanup
-    (menu-bar-mode 1)
-    (tool-bar-mode -1)
-    (scroll-bar-mode -1)
-    (show-paren-mode 1)
-    (electric-pair-mode 1)
-    (column-number-mode 1)
-    (global-display-line-numbers-mode 1)
-
-    ;; C style
-    (setq c-default-style "linux"
-          c-basic-offset 4)
-
+      ;; Theme
+      (load-theme 'modus-vivendi t)
     (add-hook 'c-mode-hook
               (lambda ()
-                (setq c-basic-offset 4
-                      indent-tabs-mode nil)))
+                (add-hook 'before-save-hook #'clang-format-buffer nil t)))
+      ;; Small UI cleanup
+      (menu-bar-mode 1)
+      (tool-bar-mode -1)
+      (scroll-bar-mode -1)
+      (show-paren-mode 1)
+      (electric-pair-mode 1)
+      (column-number-mode 1)
+      (global-display-line-numbers-mode 1)
 
-    (add-hook 'c++-mode-hook
-              (lambda ()
-                (setq c-basic-offset 4
-                      indent-tabs-mode nil)))
+      ;; C style
+      (setq c-default-style "linux"
+            c-basic-offset 4)
 
-    ;; Don't show line numbers in some buffers where they are annoying
-    (dolist (mode '(term-mode-hook
-                    shell-mode-hook
-                    eshell-mode-hook
-                    dired-mode-hook))
-      (add-hook mode (lambda () (display-line-numbers-mode 0))))
+      (add-hook 'c-mode-hook
+                (lambda ()
+                  (setq c-basic-offset 4
+                        indent-tabs-mode nil)))
+
+      (add-hook 'c++-mode-hook
+                (lambda ()
+                  (setq c-basic-offset 4
+                        indent-tabs-mode nil)))
+
+      ;; Don't show line numbers in some buffers where they are annoying
+      (dolist (mode '(term-mode-hook
+                      shell-mode-hook
+                      eshell-mode-hook
+                      dired-mode-hook))
+        (add-hook mode (lambda () (display-line-numbers-mode 0))))
   '';
 
   # Ensure Emacs loads it
